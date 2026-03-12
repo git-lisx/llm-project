f3576daa41d799c98e2245ac71d64105a5f7d0ad
lishixian <71492291@qq.com>
2026-03-12 08:55:45 +0800

    feat: 增加deepseek流式返回

--- openai_deepseek.py
+++ openai_deepseek.py
@@ -6,13 +6,26 @@ client = OpenAI(
     api_key=os.environ.get('DEEPSEEK_API_KEY'),
     base_url="https://api.deepseek.com")
 
+# stream = False
+stream = True
+
 response = client.chat.completions.create(
     model="deepseek-chat",
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
         {"role": "user", "content": "你是谁？"},
     ],
-    stream=False
+    stream=stream
 )
 
-print(response.choices[0].message.content)
+if stream:
+    for chunk in response:
+        delta = chunk.choices[0].delta
+        if not delta or delta.content is None:
+            continue
+        print(delta.content, end="", flush=True)
+    print()
+else:
+    print(response.choices[0].message.content)
+
+print()
